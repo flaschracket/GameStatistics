8e144a41ac61c6d8d12fe3e0b9a26abf32569ef2
flaschracket <52179120+flaschracket@users.noreply.github.com>
2021-01-15 16:37:04 +0100

    push1

--- module1.py
+++ module1.py
@@ -2,16 +2,23 @@ from GameObjects.Player import Player
 from GameObjects.EventCards import *
 from random import randrange
 
+
+
+CurrentStep = 0
+sumplayedEC = 0
+PlayedCardsSet = {0}
+
 firstplayer = Player()
-GameStep = 0
-PlayedCards= ''
 EC = EventCards()
 
 def SelectEC(self):
     choosingEC= 0
-    choosingEC = randrange(10)
-    self.PlayedCards= PlayedCards+','+str(choosingEC)
+    while choosingEC in PlayedCardsSet:
+        choosingEC = randrange(10)
+        """        self.PlayedCards= PlayedCards+','+str(choosingEC)"""
+    PlayedCardsSet.add(choosingEC)
     return choosingEC
+
 def TestPrint():
     print("Hello From Module")          
     print("list(DefinedEnums.EventCardType)  has error to print")
@@ -19,13 +26,15 @@ def TestPrint():
     firstplayer.printstatus()
 
 def GoForward(self):
-    self.GameStep = self.GameStep+1
-    choosingEC=SelectEC(self)
+    self.CurrentStep = self.CurrentStep+1
+    choosingEC = SelectEC(self)
     CallECFunc(choosingEC)
+    self.sumplayedEC = self.sumplayedEC + 1
+    if (self.sumplayedEC == EC.totalEventCards):
+        PlayedCardsSet = {}
 
 def CallECFunc(EventCardNummber):
     print('Hello from call funtion')
-    
     """calling a function with making its name as string"""
     FuncName = 'ECFunc'+ str(EventCardNummber)
     funcresult = getattr(EC, FuncName)()
